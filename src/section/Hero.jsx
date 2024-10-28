53a28c6b7759c1fcfdf9146e0ae6e55145f8c89f
Devika Harshey <144525325+devikaharshey@users.noreply.github.com>
2024-10-28 10:05:08 +0530

    Update Hero.jsx

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                 <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
                     Hi, I am Devika  <span className="waving-hand">👋</span>
                 </p>
-                <p className="hero_tag text-gray_gradient">A 3rd Year CSE Student</p>
+                <p className="hero_tag text-gray_gradient">Welcome To My Portfolio! :)</p>
             </div>
 
             <div className="w-full h-full absolute inset-0">
